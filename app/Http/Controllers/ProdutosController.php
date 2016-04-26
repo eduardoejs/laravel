8cf3d18d15898bc01967b39e06bbda874cf52e15
Eduardo Silva <eduardo.ejs@hotmail.com>
2016-04-26 14:30:50 -0300

    Adicionando middleware no construtor da classe

--- app/Http/Controllers/ProdutosController.php
+++ app/Http/Controllers/ProdutosController.php
@@ -8,6 +8,12 @@ use EJS\Produto;
 
 class ProdutosController extends Controller
 {
+    public function __construct() 
+    {
+        $this->middleware('auth');
+    }
+
+
     public function index()
     {
         $produtos = Produto::all();
